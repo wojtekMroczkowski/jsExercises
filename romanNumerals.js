57d5f74f5f8bcaa4fea6437d16410e13433a8a3f
wimedia <wojtek.mroczkowski@wimedia.pl>
2020-04-02 22:25:32 +0200

    Add The Hamming Distance exercise

--- romanNumerals.js
+++ romanNumerals.js
@@ -35,9 +35,18 @@ Precondition: 0 < number < 4000
 */
 
 function romanNumerals(number) {
-    return "";
-}
-
+    if (isNaN(number)) return NaN;
+    const romans = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1};
+    let roman = '';
+    let i;
+    for ( i in romans ) {
+        while ( number >= romans[i] ) {
+          roman += i;
+          number -= romans[i];
+        }
+      }
+      return roman;
+    }
 
     console.log(romanNumerals(6), 'VI', "First");
     console.log(romanNumerals(76), 'LXXVI', "Second");
